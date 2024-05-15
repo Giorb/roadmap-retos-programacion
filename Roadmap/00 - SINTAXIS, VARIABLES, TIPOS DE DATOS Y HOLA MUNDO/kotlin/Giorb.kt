ab1d0106cc1a3cf63a43f987aa921607cfec2c23
Giorb <giorb1418@gmail.com>
2024-05-14 19:06:26 -0600

    Giorb.kt

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/kotlin/Giorb.kt
@@ -0,0 +1,39 @@
+ EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+//Hola a todos! https://kotlinlang.org/
+/*Mi primer 
+comentario de 
+varias líneas*/
+
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+var miPrimerVariable: String = "Hola Mundo"
+
+val miPrimerConstante: Int = 20
+
+ * - Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...).
+//Entero
+var miEntero: Int = 5
+//Long
+var miLong: Long = 2000000
+//Flotante
+var miFlotante: Float = 3.1416
+//Double
+var miDoble: Double = 16.23242526
+//Carácter
+var miChar: Char = 'b'
+//Booleano
+var miBooleano = Boolean = False
+
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ fun main() {
+   val mensaje = "¡Hola, Kotlin!"
+   println(mensaje)
+ 
+
+println("Entero: $miEntero")
+println("Long: $miLong")
+println("Flotante: $miFlotante")
+println("Double: $miDoble")
+println("Carácter: $miChar")
+println("Booleano: $miBooleano")
+}
